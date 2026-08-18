be9c25ecf206752ecc9dcb53e2f4ab503b380e85
Roshan <roshannavale7@gmail.com>
2026-08-18 17:44:02 +0530

    css change

--- js/scene5.js
+++ js/scene5.js
@@ -1,41 +1,30 @@
-const cards=document.querySelectorAll(".memory-card");
+const cards = document.querySelectorAll(".memory-card");
 
-const counter=document.getElementById("photoCounter");
+const counter = document.getElementById("photoCounter");
 
-// sequential animation 
-document.addEventListener("scene5",()=>{
+// sequential animation
+document.addEventListener("scene5", () => {
+  cards.forEach((card) => {
+    card.classList.remove("show");
+  });
 
-cards.forEach(card=>{
+  let index = 0;
 
-card.classList.remove("show");
+  const interval = setInterval(() => {
+    cards[index].classList.add("show");
 
+    counter.innerHTML =
+      // "Memory "+(index+1)+" ❤️";
+
+      index++;
+
+    if (index === cards.length) {
+      clearInterval(interval);
+
+      setTimeout(() => {
+        nextScene();
+      }, 5000);
+    }
+  }, 2000);
 });
-
-let index=0;
-
-const interval=setInterval(()=>{
-
-cards[index].classList.add("show");
-
-counter.innerHTML=
-
-"Memory "+(index+1)+" ❤️";
-
-index++;
-
-if(index===cards.length){
-
-clearInterval(interval);
-
-setTimeout(()=>{
-
-nextScene();
-
-},5000);
-
-}
-
-},2000);
-
-});
-// end sequential animation 
+// end sequential animation
